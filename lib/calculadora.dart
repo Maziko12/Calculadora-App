80c87e45f7def0c4f912a97287d09da5fc9cc000
henriquecenoviczleo <henriquecenoviczleo@gmail.com>
2025-02-01 14:25:47 +0000

    Uhuuuul

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -1,5 +1,10 @@
 import 'package:expressions/expressions.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:core';
+
+// Participantes: Leonardo Henrique Cenovicz, João Paulo Silva Brito, Ana Emilia E. G. Z. Brito,
+// Leonardo Correia Valentim
 
 class Calculadora extends StatefulWidget {
   const Calculadora({super.key});
@@ -13,11 +18,49 @@ class _CalculadoraState extends State<Calculadora> {
   String _expressao = '';
   String _resultado = '';
 
+  double calcularSeno(double x) {
+    return sin(x);
+  }
+
+  double calcularCosseno(double x) {
+    return cos(x);
+  }
+
+  double calcularTangente(double x) {
+    return tan(x);
+  }
+
+  double calcularMedia(List<double> numeros) {
+    double soma = numeros.reduce((a, b) => a + b);
+    return soma / numeros.length;
+  }
+
+  num exponenciar(double base, double escala) {
+    return pow(base, escala);
+  }
+
   void _pressionarBotao(String valor) {
     setState(() {
       if (valor == _limpar) {
         _expressao = '';
         _resultado = '';
+      } else if (valor == 'seno') {
+        _resultado = calcularSeno(double.parse(_expressao)).toString();
+      } else if (valor == 'cos') {
+        _resultado = calcularCosseno(double.parse(_expressao)).toString();
+      } else if (valor == 'tan') {
+        _resultado = calcularTangente(double.parse(_expressao)).toString();
+      } else if (valor == 'média') {
+        List<double> numeros =
+            _expressao.split(',').map((e) => double.parse(e.trim())).toList();
+        _resultado = calcularMedia(numeros).toString();
+      } else if (valor == 'expo') {
+        List<String> partes = _expressao.split(',');
+        _resultado =
+            exponenciar(
+              double.parse(partes[0]),
+              double.parse(partes[1]),
+            ).toString();
       } else if (valor == '=') {
         _calcularResultado();
       } else {
@@ -28,22 +71,21 @@ class _CalculadoraState extends State<Calculadora> {
 
   void _calcularResultado() {
     try {
-      _resultado = _expressao.split('=').last;
-      _resultado = _avaliarExpressao(_expressao).toString();
-      _resultado = _resultado.replaceAll('.', ',');
+      _resultado = _avaliarExpressao(
+        _expressao,
+      ).toString().replaceAll('.', ',');
     } catch (e) {
       _resultado = 'Não foi possível calcular';
     }
   }
 
   double _avaliarExpressao(String expressao) {
-    expressao = expressao.replaceAll('x', '*');
-    expressao = expressao.replaceAll('÷', '/');
-    expressao = expressao.replaceAll(',', '.');
-    // Avaliar a expressao com a biblioteca expressions
+    expressao = expressao
+        .replaceAll('x', '*')
+        .replaceAll('÷', '/')
+        .replaceAll(',', '.');
     ExpressionEvaluator avaliador = const ExpressionEvaluator();
-    double resultado = avaliador.eval(Expression.parse(expressao), {});
-    return resultado;
+    return avaliador.eval(Expression.parse(expressao), {});
   }
 
   Widget _botao(String valor) {
@@ -85,7 +127,12 @@ class _CalculadoraState extends State<Calculadora> {
               _botao('='),
               _botao('+'),
               _botao('('),
-              _botao(')')
+              _botao(')'),
+              _botao('seno'),
+              _botao('cos'),
+              _botao('tan'),
+              _botao('média'),
+              _botao('expo'),
             ],
           ),
         ),
